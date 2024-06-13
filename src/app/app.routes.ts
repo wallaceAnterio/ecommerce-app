91b9c78557869546178f7b777c683ad8ce605f97
WallaceAnterio <wallacesanterio@gmail.com>
2024-06-13 20:22:09 +0100

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { ProductsComponent } from './components/products/products.component';
+import { CartComponent } from './components/cart/cart.component';
+
+export const routes: Routes = [
+   { path: '', redirectTo: 'products', pathMatch: 'full' },
+   { path: 'products', component: ProductsComponent },
+   { path: 'cart', component: CartComponent },
+];
